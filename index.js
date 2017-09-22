f0cd1f5f450ce09eb0cd33e747b96155d992562b
Chuck <chuckcipione@yahoo.com>
2017-09-22 12:22:48 -0500

    node-assessment

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const session = require('express-session');
+// const cors = require('cors');
+const userCtrl = require('./userCtrl');
+const userData = require('./userData.json');
+
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+app.get('/api/users', function(req, res, next){
+    console.log(userCtrl.getUsers(req.query))
+    res.status(200).send(userCtrl.getUsers(req.query))
+  });
+
+app.get('/api/users/:userId', function(req, res, next){
+  if(userCtrl.getUserId(req.params).status === 404){
+    return res.status(404).json(null)
+  }
+  res.status(200).json(userCtrl.getUserId(req.params))
+})
+
+app.get('/api/admins', function(req, res, next){
+    res.status(200).send(userCtrl.getAdmins())
+  })
+
+app.get('/api/nonadmins', function(req, res, next){
+    res.status(200).send(userCtrl.getNonAdmins())
+})
+
+app.get('/api/user_type/:type', function(req, res, next){
+    res.status(200).send(userCtrl.getUserType(req.params))
+})
+
+app.put('/api/users/:userId', function(req, res, next){
+    res.status(200).send(userCtrl.updateUser(req.params, req.body))
+})
+
+app.post('/api/users', function(req, res, next){
+    res.status(200).send(userCtrl.createUser(req.body))
+})
+
+app.delete('/api/users/:userId', function(req, res){
+    res.status(200).send(userCtrl.removeUser(req.params.userId))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log(`Listening on Port ${port}`)
+});
---
+++ userCtrl.js
@@ -0,0 +1,120 @@
+const userData = require('./userData.json');
+
+
+module.exports = {
+    getUsers(query){
+        if(query.favorites){
+          var users = []
+          for(var i = 0; i < userData.length; i++){
+            for(var j = 0; j < userData[i].favorites.length; j++){
+              if(userData[i].favorites[j] === query.favorites){
+                users.push(userData[i])
+              }
+            }
+          }
+          return users
+        }
+        else if(query.age){
+           
+          var users = []
+          for(var i = 0; i < userData.length; i++){
+            if(query.age == userData[i].age){
+              users.push(userData[i])
+            }
+          }
+          return users
+        }
+        else if(query.lastname){
+          var users = []
+          for(var i = 0; i < userData.length; i++){
+            if(query.lastname === userData[i].last_name){
+              users.push(userData[i])
+            }
+          }
+          return users
+        }
+        else if(query.email){
+          var user;
+          for(var i = 0; i < userData.length; i++){
+            if(query.email === userData[i].email){
+              user = userData[i]
+            }
+          }
+          return user
+        }
+        else{
+          return userData
+        } 
+        
+      },
+      getUserId(id){
+        if(!id){
+          return {status: 404, err: 'null'}
+        }
+        var user;
+        for(var i = 0; i < userData.length; i++){
+        //   console.log(userData[i].id + '----' +  id.userId)
+          if(userData[i].id === Number(id.userId)){
+            user = userData[i]
+          }
+        }
+        if(user){
+          return user
+        }else{
+          return {status: 404, err: 'null'}
+        }
+      },
+      getAdmins(){
+        var admins = []
+        for(var i = 0; i < userData.length; i++){
+          if(userData[i].type === 'admin'){
+            admins.push(userData[i])
+          }
+        }
+        return admins
+      },
+      getNonAdmins(){
+        var nonAdmins = []
+        for(var i = 0; i < userData.length; i++){
+          if(userData[i].type !== 'admin'){
+            nonAdmins.push(userData[i])
+          }
+        }
+        return nonAdmins
+      },
+      getUserType(query){
+        // console.log(query)
+        var users = []
+        for(var i = 0; i < userData.length; i++){
+          if(userData[i].type === query.type){
+            users.push(userData[i])
+          }
+        }
+        return users
+      },
+      updateUser(id, data){
+        // console.log(id.userId)
+        // console.log(data)
+        for(var i = 0; i < userData.length; i++){
+          if(userData[i].id === Number(id.userId)){
+            userData[i] = data
+          }
+        }
+        return userData
+      },
+      createUser(data){
+        var dataLength = userData.length - 1
+        var lastId = userData[dataLength].id
+        data.id = lastId + 1
+        userData.push(data)
+        return userData
+      },
+      removeUser(id){
+        for(var i = 0; i < userData.length; i++){
+          if(userData[i].id === Number(id)){
+            userData.splice(i, 1)
+          }
+        }
+        return userData
+      }
+}
